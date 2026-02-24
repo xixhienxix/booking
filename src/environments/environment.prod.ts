f61bb3b20d4538b2f4075d9b5a347d916f36728e
carlos.arturo.farias@ibm.com <Carlos.Arturo.Farias@ibm.com>
2026-02-23 23:36:05 -0600

    update cors

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,9 +4,9 @@ export const environment = {
   appVersion: 'v726demo1',
   USERDATA_KEY: 'authf649fc9a5f55',
   isMockEnabled: false,
-  //apiUrl: 'https://milobify.com',
+  apiUrl: 'https://milobify.com',
   BOOKING_APP_SECRET:'y3RB@5gX#Q6mv4eVZ2Lcz8!upG*M7daFqK$P1sRjHT9NnDbGx^Yf%WAoeLiXU0Ct',
-  apiUrl: 'http://localhost:3500',
+  //apiUrl: 'http://localhost:3500',
   //apiUrl:'https://movnext-api.herokuapp.com/api',
   hotel:'Hotel Pokemon'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@ export const environment = {
   appVersion: 'v726demo1',
   USERDATA_KEY: 'authf649fc9a5f55',
   isMockEnabled: false,
-  //apiUrl: 'https://milobify.com',
-  apiUrl: 'http://localhost:3500',
+  apiUrl: 'https://milobify.com',
+  //apiUrl: 'http://localhost:3500',
   //apiUrl:'https://movnext-api.herokuapp.com/api'
   BOOKING_APP_SECRET:'y3RB@5gX#Q6mv4eVZ2Lcz8!upG*M7daFqK$P1sRjHT9NnDbGx^Yf%WAoeLiXU0Ct',
   hotel:'Hotel Pokemon'
